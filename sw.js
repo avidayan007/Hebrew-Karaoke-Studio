2bae4c72ac416d59d4aa6ea062870ca617c9c2b0
avidayan007 <avidayan007@gmail.com>
2026-08-28 15:04:07 +0300

    Bump PWA cache for render engine v1.6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hks-v15';
-const ASSETS=['./','index.html','app.js?v=15','manifest.webmanifest'];
+const CACHE='hks-v16';
+const ASSETS=['./','index.html','app.js?v=16','manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));return}e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{if(e.request.method==='GET'){let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request)))});
