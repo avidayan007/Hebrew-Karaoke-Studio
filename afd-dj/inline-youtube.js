b7c1af1c3c93615993fe7d14a38c6b62df524236
avidayan007 <avidayan007@gmail.com>
2026-08-30 10:50:15 +0300

    Activate simplified deck and folder import v49

--- afd-dj/inline-youtube.js
+++ afd-dj/inline-youtube.js
@@ -1,4 +1,4 @@
 (()=>{
  const load=src=>new Promise((ok,no)=>{const s=document.createElement('script');s.src=src;s.async=false;s.onload=ok;s.onerror=no;document.head.appendChild(s)});
- (async()=>{try{await load('./inline-youtube-core-v43.js?v=48');await load('./mixer-polish-v43.js?v=48');await load('./layout-balance-v45.js?v=48')}catch(e){console.error('AFD DJ loader error',e)}})();
+ (async()=>{try{await load('./inline-youtube-core-v43.js?v=49');await load('./mixer-polish-v43.js?v=49');await load('./layout-balance-v45.js?v=49');await load('./ui-library-v49.js?v=49')}catch(e){console.error('AFD DJ loader error',e)}})();
 })();
