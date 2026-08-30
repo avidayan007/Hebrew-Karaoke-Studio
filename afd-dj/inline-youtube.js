c0519fc0b34e325d7c8e4f53022435d04c0918ac
avidayan007 <avidayan007@gmail.com>
2026-08-30 11:03:29 +0300

    Activate touch long press drag loading v51

--- afd-dj/inline-youtube.js
+++ afd-dj/inline-youtube.js
@@ -1,4 +1,4 @@
 (()=>{
  const load=src=>new Promise((ok,no)=>{const s=document.createElement('script');s.src=src;s.async=false;s.onload=ok;s.onerror=no;document.head.appendChild(s)});
- (async()=>{try{await load('./inline-youtube-core-v43.js?v=50');await load('./mixer-polish-v43.js?v=50');await load('./layout-balance-v45.js?v=50');await load('./ui-library-v49.js?v=50')}catch(e){console.error('AFD DJ loader error',e)}})();
+ (async()=>{try{await load('./inline-youtube-core-v43.js?v=51');await load('./mixer-polish-v43.js?v=51');await load('./layout-balance-v45.js?v=51');await load('./ui-library-v49.js?v=51')}catch(e){console.error('AFD DJ loader error',e)}})();
 })();
