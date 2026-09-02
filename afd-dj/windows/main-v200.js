fb13bfee2207815b816665fc146fe104cfb9d675
avidayan007 <avidayan007@gmail.com>
2026-09-03 01:15:34 +0300

    Use stable v201 core and local Spotify playback

--- afd-dj/windows/main-v200.js
+++ afd-dj/windows/main-v200.js
@@ -9,19 +9,19 @@ function patch(from,to,label){
 
 patch(
   "const RUNTIME_BASE_JS=readRuntime('runtime-v167.js'),RUNTIME_FIX_JS=readRuntime('runtime-v168.js'),RUNTIME_PERSIST_JS=readRuntime('runtime-v169.js'),RUNTIME_LIBRARY_JS=readRuntime('runtime-v170.js'),RUNTIME_LIBRARY_FIX_JS=readRuntime('runtime-v171.js'),RUNTIME_TEXT_SCROLL_JS=readRuntime('runtime-v172.js'),RUNTIME_KEY_JS=readRuntime('runtime-v184.js'),RUNTIME_MEDIA_PLAYLIST_JS=readRuntime('runtime-v177.js'),RUNTIME_LOAD_JS=readRuntime('runtime-v185.js'),RUNTIME_EJECT_JS=readRuntime('runtime-v175.js');",
-  "const RUNTIME_BASE_JS=readRuntime('runtime-v167.js'),RUNTIME_FIX_JS=readRuntime('runtime-v168.js'),RUNTIME_PERSIST_JS=readRuntime('runtime-v169.js'),RUNTIME_LIBRARY_JS=readRuntime('runtime-v170.js'),RUNTIME_LIBRARY_FIX_JS=readRuntime('runtime-v171.js'),RUNTIME_TEXT_SCROLL_JS=readRuntime('runtime-v172.js'),RUNTIME_KEY_JS=readRuntime('runtime-v184.js'),RUNTIME_MEDIA_PLAYLIST_JS=readRuntime('runtime-v177.js'),RUNTIME_LOAD_JS=readRuntime('runtime-v185.js'),RUNTIME_EJECT_JS=readRuntime('runtime-v175.js'),RUNTIME_UNIFIED_JS=readRuntime('runtime-v194.js'),RUNTIME_CORE_JS=readRuntime('runtime-v195.js'),RUNTIME_SPOTIFY_JS=readRuntime('runtime-v196.js'),RUNTIME_GUARD_JS=readRuntime('runtime-v200.js');",
+  "const RUNTIME_BASE_JS=readRuntime('runtime-v167.js'),RUNTIME_FIX_JS=readRuntime('runtime-v168.js'),RUNTIME_PERSIST_JS=readRuntime('runtime-v169.js'),RUNTIME_LIBRARY_JS=readRuntime('runtime-v170.js'),RUNTIME_LIBRARY_FIX_JS=readRuntime('runtime-v171.js'),RUNTIME_TEXT_SCROLL_JS=readRuntime('runtime-v172.js'),RUNTIME_KEY_JS=readRuntime('runtime-v184.js'),RUNTIME_MEDIA_PLAYLIST_JS=readRuntime('runtime-v177.js'),RUNTIME_LOAD_JS=readRuntime('runtime-v185.js'),RUNTIME_EJECT_JS=readRuntime('runtime-v175.js'),RUNTIME_UNIFIED_JS=readRuntime('runtime-v194.js'),RUNTIME_CORE_JS=readRuntime('runtime-v195.js'),RUNTIME_SPOTIFY_JS=readRuntime('runtime-v196.js'),RUNTIME_GUARD_JS=readRuntime('runtime-v200.js'),RUNTIME_STABLE_JS=readRuntime('runtime-v201.js'),SPOTIFY_PLAYBACK_LOCAL_JS='if(!window.__afdSpotifyPlaybackLocal201){window.__afdSpotifyPlaybackLocal201=1;'+fs.readFileSync(path.join(__dirname,'spotify-playback-v85-local.js'),'utf8')+'}';",
   'runtime constants'
 );
 
 patch(
   "for(const js of [RUNTIME_BASE_JS,RUNTIME_FIX_JS,RUNTIME_PERSIST_JS,RUNTIME_LIBRARY_JS,RUNTIME_LIBRARY_FIX_JS,RUNTIME_TEXT_SCROLL_JS,RUNTIME_KEY_JS,RUNTIME_MEDIA_PLAYLIST_JS,RUNTIME_LOAD_JS,RUNTIME_EJECT_JS])await w.webContents.executeJavaScript(js,true)",
-  "for(const js of [RUNTIME_BASE_JS,RUNTIME_FIX_JS,RUNTIME_PERSIST_JS,RUNTIME_LIBRARY_JS,RUNTIME_LIBRARY_FIX_JS,RUNTIME_TEXT_SCROLL_JS,RUNTIME_KEY_JS,RUNTIME_MEDIA_PLAYLIST_JS,RUNTIME_LOAD_JS,RUNTIME_EJECT_JS,RUNTIME_UNIFIED_JS,RUNTIME_CORE_JS,RUNTIME_SPOTIFY_JS,RUNTIME_GUARD_JS])await w.webContents.executeJavaScript(js,true)",
+  "for(const js of [RUNTIME_BASE_JS,RUNTIME_FIX_JS,RUNTIME_PERSIST_JS,RUNTIME_LIBRARY_JS,RUNTIME_LIBRARY_FIX_JS,RUNTIME_TEXT_SCROLL_JS,RUNTIME_KEY_JS,RUNTIME_MEDIA_PLAYLIST_JS,RUNTIME_LOAD_JS,RUNTIME_EJECT_JS,RUNTIME_CORE_JS,SPOTIFY_PLAYBACK_LOCAL_JS,RUNTIME_SPOTIFY_JS,RUNTIME_GUARD_JS,RUNTIME_STABLE_JS])await w.webContents.executeJavaScript(js,true)",
   'inject order'
 );
 
 patch(
   "w.loadURL('https://afd-dj.vercel.app/workstation.html?v=186&t='+Date.now());",
-  "w.loadURL('https://afd-dj.vercel.app/workstation.html?v=200&t='+Date.now());",
+  "w.loadURL('https://afd-dj.vercel.app/workstation.html?v=201&t='+Date.now());",
   'shell cache version'
 );
 
