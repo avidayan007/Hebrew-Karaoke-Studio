a68e092bc7ad85ac410184a8c30b07218efa2e9f
avidayan007 <avidayan007@gmail.com>
2026-09-04 00:25:19 +0300

    Add v1.5.26 Side View autoplay fix and GPU-friendly Chromium flags

---
+++ afd-dj/windows/main-v226.js
@@ -0,0 +1,17 @@
+const fs=require('fs');
+const path=require('path');
+const electron=require('electron');
+const {app}=electron;
+const {patchText}=require('./patch-v226.js');
+
+app.commandLine.appendSwitch('autoplay-policy','no-user-gesture-required');
+app.commandLine.appendSwitch('enable-gpu-rasterization');
+app.commandLine.appendSwitch('enable-zero-copy');
+
+const rawRead=fs.readFileSync.bind(fs);
+fs.readFileSync=function(p,...args){
+  const out=rawRead(p,...args),file=path.basename(String(p));
+  return patchText(file,out);
+};
+
+require('./main-v225.js');
